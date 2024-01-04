bc73e3581b6ef3fdbf2003c063388d87f1e96829
fletech <facundogarcia.ar@gmail.com>
2024-01-04 15:16:22 +0100

    update api

--- app.js
+++ app.js
@@ -8,12 +8,11 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-  })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(() => console.log("Connected to database."))
-  .catch(() => {
+  .catch((e) => {
     console.log("Cannot connect to database. Exiting.");
+    console.log(e);
     process.exit();
   });
 
